41c5078e172d2e4209f7196b4d3a15ef58ab4b77
tanishi109 <hayakawa@goodpatch.com>
2017-07-28 13:38:33 +0900

    :tada: bind keyboard events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 export class Player {
   entry: number;
   name: string;
+  commands: number[];
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'My First Angular App';
   player: Player = {
     entry: 1,
     name: 'Player',
+    commands: [],
+  };
+  @HostListener('keydown') handleKey($event) {
+    console.log($event)
   };
 }
