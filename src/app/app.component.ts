e40d0f72d60394b9e67ade2bad291f6566c05759
tanishi109 <hayakawa@goodpatch.com>
2017-07-28 13:21:57 +0900

    display Player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Player {
+  entry: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My First Angular App';
+  player: Player = {
+    entry: 1,
+    name: 'Player',
+  };
 }
