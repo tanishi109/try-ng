a13c2539a8c12b69f46909b2e77fad10409f87ad
tanishi109 <hayakawa@goodpatch.com>
2017-07-31 00:08:34 +0900

    Add GamePad Manager from https://github.com/tanishi109/try-3d

---
+++ src/app/GamePadManager.ts
@@ -0,0 +1,44 @@
+import {times, compact} from 'lodash';
+
+type handler = (gp: Gamepad) => void;
+
+export default class GamepadManager {
+  private handleGamepad: handler;
+
+  constructor(handleGamepad: handler) {
+    window.addEventListener('gamepadconnected', this.onConnect, false);
+    window.addEventListener('gamepaddisconnected', this.onDisconnect, false);
+
+    this.handleGamepad = handleGamepad;
+    this.update();
+  }
+
+  private update() {
+    this.updateGamepads();
+
+    requestAnimationFrame(this.update.bind(this));
+  }
+
+  private updateGamepads() {
+    const gamepads = this.getGamepads();
+
+    gamepads.forEach(this.handleGamepad);
+  }
+
+  private getGamepads() {
+    const gamepads = navigator.getGamepads();
+
+    return compact(
+      times(gamepads.length)
+        .map((index) => gamepads[index]),
+    );
+  }
+
+  private onConnect(e: GamepadEvent) {
+    console.log('*** CONNECTED ***'); // tslint:disable-line no-console
+  }
+
+  private onDisconnect(e: GamepadEvent) {
+    console.log('*** DISCONNECTED ***'); // tslint:disable-line no-console
+  }
+}
