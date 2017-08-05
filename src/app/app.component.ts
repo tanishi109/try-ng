607184e38b7137ac4992aeba89478bfe9519dfd8
tanishi109 <hayakawa@goodpatch.com>
2017-08-05 19:17:23 +0900

    display arrow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -110,4 +110,23 @@ export class AppComponent {
     const keyCode = $event.keyCode;
     this.keyEvents.next(keyCode);
   };
+
+  getMoveView() {
+    return this.player.move.map((command) => {
+      return commandName[command];
+    });
+  }
 }
+
+const commandName = {
+  '1': '↙',
+  '2': '↓',
+  '3': '︎↘︎',
+  '4': '←',
+  // '5': '',
+  '6': '→',
+  '7': '↖︎',
+  '8': '↑ ',
+  '9': '↗︎',
+  'p': 'P',
+};
