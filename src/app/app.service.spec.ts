717565af53c60f2a7aedeba14c1ea03815f74ab0
tanishi109 <hayakawa@goodpatch.com>
2017-08-05 14:32:44 +0900

    :green_heart: Add test for rotate() method

---
+++ src/app/app.service.spec.ts
@@ -0,0 +1,65 @@
+import { AppService, Commands, CommandDial } from './app.service';
+import {times} from 'lodash';
+
+describe('AppService', () => {
+  let s: AppService;
+
+  beforeEach(() => {
+    s = new AppService();
+  });
+
+  it('should ***', (() => {
+    const command = s.getCommandFromKeyCode(37);
+
+    expect(command).toBe(Commands.Left);
+  }));
+});
+
+describe('CommandDial', () => {
+  describe('start rotate() from Down command', () => {
+    let c: CommandDial;
+
+    beforeEach(() => {
+      c = new CommandDial(Commands.Down);
+    });
+
+    it('should return command clockwise', () => {
+      expect(c.command).toBe(Commands.Down);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.LeftDown);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.Left);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.LeftUp);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.Up);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.RightUp);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.Right);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.RightDown);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.Down);
+
+      c.rotate(true, 1);
+      expect(c.command).toBe(Commands.LeftDown);
+    });
+
+    it('should return twice next command when step specified', () => {
+      expect(c.command).toBe(Commands.Down);
+
+      c.rotate(true, 2);
+      expect(c.command).toBe(Commands.Left);
+    });
+
+  });
+});
