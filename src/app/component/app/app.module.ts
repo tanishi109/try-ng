970866fa7eadf8135010e3c8926ab5dfa6991c2e
tanishi109 <hayakawa@goodpatch.com>
2017-08-16 19:55:30 +0900

    :+1: Create stroke component and show in app

--- src/app/component/app/app.module.ts
+++ src/app/component/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CommandComponent } from '../command/command.component';
+import { StrokeComponent } from '../stroke/stroke.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CommandComponent,
+    StrokeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/stroke/stroke.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, AfterViewInit } from '@angular/core';
+import { Commands } from 'app/domain/command';
+import { innerSize } from '../command/command.component';
+
+const pos = {
+  [Commands.LeftUp]: [0, 0],
+  [Commands.Up]: [1, 0],
+  [Commands.RightUp]: [2, 0],
+
+  [Commands.Left]: [0, 1],
+  [Commands.Neutral]: [1, 1],
+  [Commands.Right]: [2, 1],
+
+  [Commands.LeftDown]: [0, 2],
+  [Commands.Down]: [1, 2],
+  [Commands.RightDown]: [2, 2],
+};
+
+@Component({
+  selector: 'stroke-component',
+  templateUrl: './stroke.component.html',
+  styleUrls: ['./stroke.component.css'],
+})
+export class StrokeComponent implements AfterViewInit {
+  @Input() command: string;
+  rootStyle = {};
+
+  ngAfterViewInit() {
+    this.rootStyle = {
+      'left.px': pos[this.command][0] * innerSize,
+      'top.px': pos[this.command][1] * innerSize,
+    };
+  }
+}
