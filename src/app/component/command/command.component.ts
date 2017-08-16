26a84acd132ebd79472f1d47558c7c689b931243
tanishi109 <hayakawa@goodpatch.com>
2017-08-16 19:54:15 +0900

    :recycle: Use vars for commandComponent size

--- src/app/component/command/command.component.ts
+++ src/app/component/command/command.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 
-
+export const innerSize = 30;
 export const size = 94;
 export const topMargin = 16;
 export const outerSize = size + topMargin;
@@ -12,6 +12,10 @@ export const outerSize = size + topMargin;
 })
 export class CommandComponent {
   @Input() command: string;
+  innerStyle = {
+    'width.px': innerSize,
+    'height.px': innerSize,
+  };
   wrapperStyle = {
     'width.px': size,
     'height.px': size,
